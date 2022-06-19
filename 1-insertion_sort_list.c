14425f6edf3b4f6b832c6c1c0784c94c1c0b59ab
jhojan <jhojan_perlaza123>
2022-06-19 14:41:02 -0500

    [ADD] Files of task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+listint_t *swap_nodes(listint_t *current, int *flag)
+{
+	listint_t *tmp = current->next;
+
+	tmp->prev = NULL;
+	if (current->prev != NULL)
+	{
+		tmp->prev = current->prev;
+		tmp->prev->next = tmp;
+	}
+	else if (current->prev == NULL)
+		*flag = 1;
+	
+	current->prev = tmp;
+	current->next = tmp->next;
+	tmp->next = current;
+	if (current->next != NULL)
+		current->next->prev = current;
+	
+	return (tmp);
+}
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = *list;
+	int flag;
+
+	while (current->next)
+	{
+		flag = 0;
+		if (current->n > current->next->n)
+		{
+			current = swap_nodes(current, &flag);
+			if (flag == 1)
+				*list = current;
+			print_list(*list);
+			while (current->prev != NULL && current->prev->n > current->n)
+			{
+				swap_nodes(current->prev, &flag);
+				if (flag == 1)
+					*list = current;
+				print_list(*list);
+			}
+		}
+		current = current->next;
+	}
+}
