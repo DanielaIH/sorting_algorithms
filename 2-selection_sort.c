041a63a6686fa703d2cf4a0d577644bf6f7b5ac1
DanielaIH <4427@holbertonschool.com>
2022-06-20 14:40:59 -0500

    [UPDATE] format

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,27 +10,27 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t cont = 0, min = 0, i = 0;
-    int tmp = 0;
+	size_t cont = 0, min = 0, i = 0;
+	int tmp = 0;
 
-    if (!array || !size)
-        return;
-    
-    for (i = 0; i < size; i++)
-        {
-            min = i;
-            tmp = 0;
-            for (cont = i + 1; cont < size; cont++)
-            {
-                if (array[cont] < array[i] && array[min] > array[cont])
-                    min = cont;
-            }
-            if (min != i)
-            {
-                tmp = array[i];
-                array[i] = array[min];
-                array[min] = tmp;
-                print_array(array, size);
-            }
-        }
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		tmp = 0;
+		for (cont = i + 1; cont < size; cont++)
+		{
+			if (array[cont] < array[i] && array[min] > array[cont])
+				min = cont;
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
 }
