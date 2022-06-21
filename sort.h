e6d8238343e28d5bcd66873646ba0199848e3989
jhojan <jhojan_perlaza123>
2022-06-21 10:34:48 -0500

    [ADD] file .h

--- sort.h
+++ sort.h
@@ -24,6 +24,8 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
 listint_t *swap_nodes(listint_t *current, int *flag);
+void quick_sort(int *array, size_t size);
+int partition(int *arr, int low, int high, size_t size);
+void quicksort(int *arr, int low, int high, size_t size);
 #endif
